70d0330a0b3fe6931523754447ea5be41d99a398
Saurav s Mehta <sauravmehta631@gmail.com>
2025-09-26 08:14:18 +0530

    basic hashmap op

--- Hashing/hashmap.java
+++ Hashing/hashmap.java
@@ -1,7 +1,38 @@
 package Hashing;
-
+import java.util.*;
 public class hashmap {
     public static void main(String args[]){
-        System.out.println("HII");
+        //Create
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //insert O(1)
+        hm.put("India",100);
+        hm.put("China",150);
+        hm.put("US",50);
+
+        System.out.println(hm);
+
+        //get - O(1)
+        System.out.println(hm.get("sdas'"));
+        int population = hm.get("India");
+        System.out.println(population);
+
+        //containKey - O(1)
+        System.out.println(hm.containsKey("India"));
+        System.out.println(hm.containsKey("Nepal"));
+
+        //remove - O(1)
+        System.out.println(hm.remove("India"));
+        System.out.println(hm);
+        System.out.println(hm.remove("Nepal"));
+        System.out.println(hm);
+
+
+        //other operation
+        System.out.println(hm.keySet()); //set of keys
+        System.out.println(hm.values()); //set of values
+        System.out.println(hm.size()); //size of hashMap
+        System.out.println(hm.isEmpty()); //return true/false
+
     }
 }
