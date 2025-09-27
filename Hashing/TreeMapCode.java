e8b1e887f15a9ab6e81cca205eac27c2045ca841
Saurav s Mehta <sauravmehta631@gmail.com>
2025-09-27 07:11:31 +0530

    treeMap

---
+++ Hashing/TreeMapCode.java
@@ -0,0 +1,14 @@
+package Hashing;
+import java.util.*;
+
+public class TreeMapCode {
+    
+    public static void main(String args[]){
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        tm.put("India", 100);
+        tm.put("China", 300);
+        tm.put("US", 230);
+
+        System.out.println(tm);
+    }
+}
