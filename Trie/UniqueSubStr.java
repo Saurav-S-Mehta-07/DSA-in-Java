6f306d6f9322b66d04496906f715a8088d81c8be
Saurav s Mehta <sauravmehta631@gmail.com>
2025-09-29 09:52:21 +0530

    unique substring

--- Trie/UniqueSubStr.java
+++ Trie/UniqueSubStr.java
@@ -14,29 +14,43 @@ public class UniqueSubStr {
     }
 
     public static Node root = new Node();
-    public static int count = 1;
 
     public static void insertInTree(String word){
         Node curr = root;
         for(int i =0; i<word.length(); i++){
             int idx = word.charAt(i) - 'a';
             if(curr.children[idx] ==  null){
-                count++;
                 curr.children[idx] = new Node();
             }
             curr = curr.children[idx];
         }
         curr.eow = true;
     }
+
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int count = 0;
+        for(int i = 0; i<26; i++){
+            if(root.children[i] != null){
+                count+= countNodes(root.children[i]);
+            }
+        }
+        return count+1;
+    }
+
+
     public static void main(String args[]){
         
        String str = "ababa";
-
+        
+       //suffix
        for(int i = 0; i<str.length(); i++){
             insertInTree(str.substring(i));
        }
 
-       System.out.println("total unique substring : " + count);
+       System.out.println("total unique substring : " + countNodes(root));
 
     }
 
