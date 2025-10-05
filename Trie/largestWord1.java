08564b3c158e1f19fa3a5e6d6d22dda8054779a2
Saurav s Mehta <sauravmehta631@gmail.com>
2025-10-05 07:59:10 +0530

    added

---
+++ Trie/largestWord1.java
@@ -0,0 +1,62 @@
+package Trie;
+
+public class largestWord1 {
+
+    public static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+            for(int i = 0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int i = 0; i<word.length(); i++){
+            int idx = word.charAt(i)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+
+    public static String str = "";
+
+    public static void largestWord1(String word){
+         Node curr = root;
+         boolean valid = true;
+         for(int i = 0; i<word.length(); i++){
+            int idx = word.charAt(i)-'a';
+            if(curr.children[idx]!=null && curr.children[idx].eow == false){
+                valid = false; 
+                break;
+            }
+            curr = curr.children[idx];
+         }
+
+         if(valid){
+            if(word.length()>str.length() || (word.length() == str.length() && word.compareTo(str)<0)){
+                str = word;
+            }
+         }
+    }
+
+    public static void main(String[] args){
+        String[] words = {"w", "wo", "wor", "word","worl", "world"};
+        for(int i = 0; i<words.length; i++){
+            insert(words[i]);
+        }
+        for(int i= 0; i<words.length; i++){
+            largestWord1(words[i]);
+        }
+        System.out.println(str);
+    }
+}
