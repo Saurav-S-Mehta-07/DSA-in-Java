e55d8247b67106a77132e3bfbab341d60376df2a
Saurav s Mehta <sauravmehta631@gmail.com>
2025-10-26 08:37:16 +0530

    added

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,49 @@
+package DP;
+import java.util.*;
+//count ways to reach the nth stair.
+//The person can climb either 1 stair or 2 stairs at a time.
+
+public class ClimbingStairs {
+
+    //recursion
+    public static int countWays(int n){
+          if(n<0) return 0;
+          if(n==0) return 1;
+          return countWays(n-1)+countWays(n-2);
+    }
+
+    //recursion (memoization)
+    public static int countWaysM(int n, int[] ways){
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(ways[n]!=-1) return ways[n];
+        ways[n] =  countWaysM(n-1, ways) + countWaysM(n-2, ways);
+        return ways[n];
+    }
+
+    //tabulation
+    public static int countWaysT(int n, int[] ways){
+        if(n<0) return 0;
+        ways[0] = 1;
+        ways[1] = 1;
+        for(int i = 2; i<=n; i++){
+            ways[i] = ways[i-1] + ways[i-2];
+        } 
+        return ways[n];
+    }
+    public static void main(String[] args){
+        //recursion
+        // System.out.println(countWays(5));
+
+        //memoization
+        int n = 5;
+        int ways[] = new int[n+1];
+        Arrays.fill(ways, -1);
+        // System.out.println(countWaysM(n, ways));
+
+        //tabulation
+        System.out.println(countWaysT(n, ways));
+        
+    }
+
+}
