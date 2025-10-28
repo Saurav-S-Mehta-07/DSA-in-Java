8a46474e0c27e286ffd8595a6adbcdeca4785eea
Saurav s Mehta <sauravmehta631@gmail.com>
2025-10-28 20:14:17 +0530

    added

--- DP/LCSubStr.java
+++ DP/LCSubStr.java
@@ -1,27 +1,13 @@
 package DP;
 
 public class LCSubStr {
- 
-    static int res = 0;
-    public static int LCSubString(String s1, String s2, int n, int m){
-         if(n==0 || m==0) return 0;
-
-         if(s1.charAt(n-1) == s2.charAt(m-1)){
-            int ans1 = LCSubString(s1, s2,n-1,m-1)+1;
-            res = Math.max(res, ans1);
-            return ans1;
-         }
-         else{
-             return 0;
-         }
-    }
 
     //tabulation
     public static int LCSubString(String s1, String s2){
         int n = s1.length();
         int m = s2.length();
         int dp[][] = new int[n+1][m+1];
-        int ans = 0;
+        int ans = 0; // because answer may or may not be in dp[n][m]
         //initialize
         for(int i= 0; i<n+1; i++) dp[i][0] = 0;
         for(int j =0; j<m+1; j++) dp[0][j] = 0;
@@ -45,10 +31,10 @@ public class LCSubStr {
         String s1 = "abcde";
         String s2 = "abgce";
 
-        System.out.println(LCSubString(s1, s2, s1.length(), s2.length())); //2
+        System.out.println(LCSubString(s1, s2)); //2
 
         s1 = "abcde";
         s2 = "abcdg";
-        System.out.println(LCSubString(s1, s2, s1.length(), s2.length())); //4
+        System.out.println(LCSubString(s1, s2)); //4
       }
 }
