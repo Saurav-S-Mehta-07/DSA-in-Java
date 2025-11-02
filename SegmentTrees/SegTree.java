0f50023558f1697ee8d29e9e0a93f955a93f1315
Saurav s Mehta <sauravmehta631@gmail.com>
2025-11-02 10:41:10 +0530

    added

--- SegmentTrees/SegTree.java
+++ SegmentTrees/SegTree.java
@@ -11,16 +11,48 @@ public class SegTree {
         return st[sti];
     }
 
-    public static void createST(int[] arr){
+    public static int[] createST(int[] arr){
         int n = arr.length;
         int[] st = new int[4*n];
         buildST(arr,st,0,0,n-1);
-        System.out.println(st[0]);
+        printST(st);
+        return st;
+    }
+
+    public static void printST(int[] st){
+        for(int i = 0; i<st.length; i++){
+            System.out.print(st[i]+ " ");
+        }
+        System.out.println();
+    }
+
+    public static int getSumUtil(int[] st, int i, int si, int sj, int qi, int qj){
+       //case 1: non overlap
+       if(qj<=si || qi>=sj) return 0;
+       //case 2: completely overlaping
+       else if(si>=qi && sj<=qj) return st[i];
+       //case 3: partial overlap
+       else{
+          int mid = (si+sj)/2;
+          int left = getSumUtil(st, 2*i+1, si, mid, qi, qj);
+          int right = getSumUtil(st, 2*i+2, mid+1, sj, qi, qj);
+          return left+right;
+       }
+    }
+
+
+    public static int getSum(int[] arr,int[] st, int qi, int qj){
+           int n = arr.length;
+        return getSumUtil(st, 0,0,n-1, qi, qj);
     }
     
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
-        createST(arr);
+        int st[] = createST(arr);
+        int qi = 1;
+        int qj = 3;
+        int sum = getSum(arr,st, qi, qj);
+        System.out.println(sum);
     }
 }
  
