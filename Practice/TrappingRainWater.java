2b5f4bebe126ada7179fdfa8168ef9fbbac9c735
Saurav s Mehta <sauravmehta631@gmail.com>
2026-01-11 17:52:19 +0530

    trapping rainwater

---
+++ Practice/TrappingRainWater.java
@@ -0,0 +1,31 @@
+package Practice;
+
+public class TrappingRainWater {
+
+    //O(n)
+    public static void totalRailWater(int[] height){
+       int n = height.length;
+       int left[] = new int[n];
+       int right[] = new int[n];
+
+       left[0] = height[0];
+       for(int i = 1; i<n; i++){
+        left[i] = Math.max(left[i-1],height[i]);
+       }
+       right[n-1] = height[n-1];
+       for(int i = n-2; i>=0; i--){
+        right[i] = Math.max(right[i+1],height[i]);
+       }
+
+       int totalWater = 0;
+       for(int i =0; i<n; i++){
+          int currLevel = Math.min(left[i],right[i]);
+          totalWater += currLevel - height[i];
+       }
+       System.out.println("total trapped water = " + totalWater);
+    }
+    public static void main(String[] args) {
+        int height[] = {4,2,0,6,3,2,5};
+        totalRailWater(height);
+    }
+}
